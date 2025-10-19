1fbda56b15dd7bc584cf8632d587526a4f130f2c
Lingnan Xu <103618232+XuLingnan@users.noreply.github.com>
2025-10-19 19:31:43 +0800

    Delete scripts/curation/pre_retrieval.sh

--- scripts/curation/pre_retrieval.sh
+++
@@ -1,9 +0,0 @@
-export PYTHONPATH=/home/master/xulingnan/camerupt:$PYTHONPATH
-
-python src/struct_curation.py \
-    --pipeline "pre_retrieval" \
-    --n_down_sample 1000 \
-    --start_index 0 \
-    --n_docs 5 \
-    --dataset "datasets/asqa/asqa_train_4353.parquet" \
-    --output_folder "curated_data/pre_retrieved_dataset"
