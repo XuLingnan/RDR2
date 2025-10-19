183996273a319cb7a9f1b9bfa6cbf715a39466b8
Lingnan Xu <103618232+XuLingnan@users.noreply.github.com>
2025-10-19 19:31:30 +0800

    Delete scripts/curation/curate_query_centric.sh

--- scripts/curation/curate_query_centric.sh
+++
@@ -1,9 +0,0 @@
-export PYTHONPATH=/home/master/xulingnan/camerupt:$PYTHONPATH
-
-python src/struct_curation.py \
-    --pipeline "query_centric" \
-    --n_down_sample 25000 \
-    --start_index 0 \
-    --n_docs 5 \
-    --dataset "curated_data/pre_retrieved_dataset/asqa_train_4353_pre_retrieval.parquet" \
-    --output_folder "curated_data/query_centric_dt/pre_retrieved_wiki"
